de3c371f1b217b071f8f1ebca99c0031bee726e0
Kelvin Tran <kelvin.tran699@gmail.com>
2026-09-01 13:26:58 +0200

    added dotenv package in composer

--- send-mail.php
+++ send-mail.php
@@ -1,6 +1,9 @@
 <?php
     // autoload Composer dependencies
     require __DIR__ . '/vendor/autoload.php';
+    // Load environment variables from .env file
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
     if ($_SERVER['REQUEST_METHOD'] =='POST') {
         $topic = $_POST['topic'];
@@ -20,14 +23,19 @@
         $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
-        $mail->setFrom($email, 'Die Brezel Contact Form');
-        $mail->addAddress($storeEmail);
+        $mail->setFrom($email, $name); // Set the sender of the email to the user's email and name
+        $mail->addAddress($storeEmail); // Our email
+        $mail->addReplyTo($email, $name);
         $mail->Subject = $topic;
         $mail->Body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
 
         if(!$mail->send()) {
             echo 'Message could not be sent.';
             echo 'Mailer Error: ' . $mail->ErrorInfo;
+            // Debug - see what's in $_POST
+            var_dump($_POST);
+            var_dump($storeEmail);
+            die();
         } else {
             echo 'Message has been sent';
         }
