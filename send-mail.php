579958a795a7441b76ee3bf5fe4767717f696208
Kelvin Tran <kelvin.tran699@gmail.com>
2026-09-01 13:44:40 +0200

    fixed bug in email form - new bug is cant connect to smtp

--- send-mail.php
+++ send-mail.php
@@ -5,26 +5,24 @@
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
+
     if ($_SERVER['REQUEST_METHOD'] =='POST') {
         $topic = $_POST['topic'];
         $name= $_POST['name'];
         $email= $_POST['email'];
         $message= $_POST['email-content'];
 
-        $storeEmail = getenv('EMAIL');
-        $emailPasswordFromEnv = getenv('EMAIL_PW');
-
         $mail = new PHPMailer\PHPMailer\PHPMailer();
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = $storeEmail;
-        $mail->Password = $emailPasswordFromEnv;
+        $mail->Username = $_ENV['EMAIL'];
+        $mail->Password = $_ENV['EMAIL_PW'];
         $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         $mail->setFrom($email, $name); // Set the sender of the email to the user's email and name
-        $mail->addAddress($storeEmail); // Our email
+        $mail->addAddress($_ENV['EMAIL']); // Our email
         $mail->addReplyTo($email, $name);
         $mail->Subject = $topic;
         $mail->Body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
@@ -32,9 +30,6 @@
         if(!$mail->send()) {
             echo 'Message could not be sent.';
             echo 'Mailer Error: ' . $mail->ErrorInfo;
-            // Debug - see what's in $_POST
-            var_dump($_POST);
-            var_dump($storeEmail);
             die();
         } else {
             echo 'Message has been sent';
